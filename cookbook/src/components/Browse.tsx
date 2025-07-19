917d6cb99aa41d37d661cde8abb4047eacde1a58
Azmo Rinsler <az.rinsler@gmail.com>
2025-07-18 20:56:56 -0400

    Hunting for more semantic crap

--- cookbook/src/components/Browse.tsx
+++ cookbook/src/components/Browse.tsx
@@ -1,45 +1,44 @@
 import * as React from "react";
+import {useEffect, useState} from "react";
 
 const Browse : () => React.JSX.Element = () => {
 
-    // const [loading, setLoading] = useState(true);
-    // const [error, setError] = useState(null);
-    // const [data, setData] = useState(null);
-    //
-    // useEffect(() => {
-    //     // this calls a Lambda which has a cold start time and may need a few seconds if it hasn't been used recently
-    //     if (loading) {
-    //         fetch("https://api.azrinsler.com/RecipeLambda", {
-    //             signal: AbortSignal.timeout(120 * 1000),
-    //             method: "POST",
-    //             headers: {
-    //                 "Content-Type": "application/json",
-    //                 "Access-Control-Allow-Origin": "https://api.azrinsler.com/RecipeLambda"
-    //             },
-    //             body: JSON.stringify({ "operation":"getRecipes" })
-    //         })
-    //         .then((response) => {
-    //             if (!response.ok) {
-    //                 throw new Error('Network response was not ok');
-    //             }
-    //             return response.json();
-    //         })
-    //         .then((jsonData) => {
-    //             if (jsonData != null) {
-    //                 console.log(jsonData);
-    //                 setData(jsonData);
-    //                 setLoading(false);
-    //             }
-    //         })
-    //         .catch((err) => {
-    //             setError(err.message);
-    //             setLoading(false);
-    //         });
-    //     }
-    // }, [data, loading] );
-    //
-    // return error ? <span>{error}</span> : loading ? <span>Loading</span> : <div>data goes here</div>
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [data, setData] = useState(null);
 
-    return <div>Browse Content</div>
+    useEffect(() => {
+        // this calls a Lambda which has a cold start time and may need a few seconds if it hasn't been used recently
+        if (loading) {
+            fetch("https://api.azrinsler.com/RecipeLambda", {
+                signal: AbortSignal.timeout(120 * 1000),
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Access-Control-Allow-Origin": "https://api.azrinsler.com/RecipeLambda"
+                },
+                body: JSON.stringify({ "operation":"getRecipes" })
+            })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then((jsonData) => {
+                if (jsonData != null) {
+                    console.log(jsonData);
+                    setData(jsonData);
+                    setLoading(false);
+                }
+            })
+            .catch((err) => {
+                setError(err.message);
+                setLoading(false);
+            });
+        }
+    }, [data, loading] );
+
+    return ( error ? <span>{error}</span> : loading ? <span>Loading</span> : <div>{data}</div> )
 }
 export default Browse
