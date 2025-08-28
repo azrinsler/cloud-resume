21428743d7e24c760cea3e47d949acdde742617d
Azmo Rinsler <az.rinsler@gmail.com>
2025-08-28 16:15:38 -0400

    Troubleshooting weird mobile bug

--- cookbook/src/components/about/AboutTechnical.tsx
+++ cookbook/src/components/about/AboutTechnical.tsx
@@ -17,9 +17,10 @@ const AboutTechnical: React.FC<AboutTechnicalProps> = (props: AboutTechnicalProp
     useEffect(() => {
         const activeImg = document.getElementById(props.images[activeSlide].props.id)
         console.log(activeImg);
-        activeImg?.scrollIntoView({behavior:'smooth',inline:'center'})
-        if (!isMobile)
+        if (!isMobile) {
+            activeImg?.scrollIntoView({behavior:'smooth',inline:'center'})
             activeImg!.style.border = '5px solid light-dark(blue,white)'
+        }
         props.images.forEach((image) => {
             const thisImg = document.getElementById(image.props.id)
             if (thisImg != activeImg) {
