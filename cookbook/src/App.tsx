be9ebc112542387cdbb7a1295ffcaf5e2786a4d9
Azmo Rinsler <az.rinsler@gmail.com>
2025-10-06 21:03:44 -0400

    trying to get users to work

--- cookbook/src/App.tsx
+++ cookbook/src/App.tsx
@@ -17,12 +17,12 @@ export function App() {
     const auth = useAuth();
 
     // this currently needs to be manually updated to match the user pool any time it gets recreated, which is not ideal
-    const signOutRedirect = () => {
-        const clientId = "4beki0cs423atvjm1kr2ojehhu";
-        const logoutUri = "https://www.azrinsler.com/";
-        const cognitoDomain = "https://cookbook-login.auth.us-east-1.amazoncognito.com";
-        window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
-    };
+    // const signOutRedirect = () => {
+    //     const clientId = "4beki0cs423atvjm1kr2ojehhu";
+    //     const logoutUri = "https://www.azrinsler.com/";
+    //     const cognitoDomain = "https://cookbook-login.auth.us-east-1.amazoncognito.com";
+    //     window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
+    // };
 
     // we'll try caching a few of these in local storage as well, so if the user reloads they don't lose their place
     const [recipeId, setRecipeId] = useState( localStorage.getItem("recipeId") || "0")
@@ -61,25 +61,25 @@ export function App() {
                 "recipeId": recipe
             })
         })
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then((json) => {
-            if (json != null) {
-                const jsonData = JSON.parse(json) as Recipe;
-                setData(jsonData);
-                console.log(jsonData);
-                setLoading(false);
-                setError(null)
-            }
-        })
-        .catch((err) => {
-            setError(err.message);
-            setLoading(false)
-        });
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then((json) => {
+                if (json != null) {
+                    const jsonData = JSON.parse(json) as Recipe;
+                    setData(jsonData);
+                    console.log(jsonData);
+                    setLoading(false);
+                    setError(null)
+                }
+            })
+            .catch((err) => {
+                setError(err.message);
+                setLoading(false)
+            });
     }, []);
 
     // I think this should set loading to true and cache our recipe id any time it changes?
@@ -100,77 +100,84 @@ export function App() {
         }
     }, [fetchRecipe, loading, recipeId, sidebarOption]);
 
-    if (auth.isLoading) {
-        return <div>Loading...</div>;
-    }
+    // if (auth.isLoading) {
+    //     return <div>Loading...</div>;
+    // }
+    //
+    // if (auth.error) {
+    //     return <div>Encountering error... {auth.error.message}</div>;
+    // }
 
-    if (auth.error) {
-        return <div>Encountering error... {auth.error.message}</div>;
-    }
-
-    if (auth.isAuthenticated) {
+    // if (auth.isAuthenticated) {
         return (
-          <>
-              <Sidebar
-                  icon={<img src={ isDarkMode ? sidebarIcon : sidebarIconBlack } alt='Sidebar Icon'></img>}
-                  title={<span style={ isMobile ? { fontSize:"x-large" } : {}}>Simple Recipes</span>}
-                  content={[
-                      <div style={ isMobile ? { fontSize:"large" } : {} } onClick={ () => { setSidebarOption("about") } }>About</div>,
-                      <div style={ isMobile ? { fontSize:"large" } : {} } onClick={ () => { setSidebarOption("new") } }>New Recipe</div>,
-                      <div style={ isMobile ? { fontSize:"large" } : {} } onClick={ () => { setSidebarOption("browse") } }>Browse Recipes</div>,
-                      <div style={ isMobile ? { fontSize:"large" } : {} } onClick={ () => { setSidebarOption("recipe") } }>Current Recipe</div>,
-                      <a   style={ isMobile ? { fontSize:"large" } : {} } href='https://github.com/azrinsler/cloud-resume/tree/main/cookbook'>GitHub</a>,
-                      <div style={ isMobile ? { fontSize:"large" } : {} }
-                           onMouseLeave={ () => { setJokeOption("") } }
-                           onMouseEnter={ () => { setJokeOption("donate") } }>
-                          { jokeOption == "donate" ? <>Don't</> : <>Donate</> }
-                      </div>,
-                      <div>
-                          <pre> Hello: {auth.user?.profile.email} </pre>
-                          <pre> ID Token: {auth.user?.id_token} </pre>
-                          <pre> Access Token: {auth.user?.access_token} </pre>
-                          <pre> Refresh Token: {auth.user?.refresh_token} </pre>
-                          <button onClick={() => auth.removeUser()}>Sign out</button>
-                      </div>
-                  ]}
-              >
-              </Sidebar>
-              <div className='flex-column' style={{flexGrow:'1', overflow:'hidden', maxHeight:'100dvh'}}>
-                  {
-                      sidebarOption == "about"
-                          ? <About></About>
-                      : sidebarOption == "new"
-                          ? <NewRecipe></NewRecipe>
-                      : sidebarOption == "browse"
-                          ? <Browse recipeCallback={fetchRecipe}></Browse>
-                      : sidebarOption == "recipe" && loading
-                          ? <Preheating></Preheating>
-                      : sidebarOption == "recipe"
-                          ? <>
-                              <RecipeCard
-                                  title={data?.title}
-                                  ingredients={data?.ingredients}
-                                  items={data?.items}
-                                  steps={data?.steps}>
-                              </RecipeCard>
-                              { error ? <><p style={{color:'red'}}>{error}</p><p style={{color:'darkgoldenrod'}}>Example Recipe</p></> : <></> }
-                            </>
-                      : <>Unknown Sidebar Option</>
-                  }
-                  <div style={{width:'100%',textAlign:'center',color:'red',position:'sticky',bottom:'0'}}>
-                      IMPORTANT: Please note that this is a DEV environment - new recipes may be lost any time the DB changes!
-                  </div>
-              </div>
-          </>
-      )
-    }
-    return (
-        <>
-            <div>
-                <button onClick={() => auth.signinRedirect()}>Sign in</button>
-                <button onClick={() => signOutRedirect()}>Sign out</button>
-            </div>
-        </>
-    );
+            <>
+                <Sidebar
+                    icon={<img src={ isDarkMode ? sidebarIcon : sidebarIconBlack } alt='Sidebar Icon'></img>}
+                    title={<span style={ isMobile ? { fontSize:"x-large" } : {}}>Simple Recipes</span>}
+                    content={[
+                        <div style={ isMobile ? { fontSize:"large" } : {} } onClick={ () => { setSidebarOption("about") } }>About</div>,
+                        <div style={ isMobile ? { fontSize:"large" } : {} } onClick={ () => { setSidebarOption("new") } }>New Recipe</div>,
+                        <div style={ isMobile ? { fontSize:"large" } : {} } onClick={ () => { setSidebarOption("browse") } }>Browse Recipes</div>,
+                        <div style={ isMobile ? { fontSize:"large" } : {} } onClick={ () => { setSidebarOption("recipe") } }>Current Recipe</div>,
+                        <a   style={ isMobile ? { fontSize:"large" } : {} } href='https://github.com/azrinsler/cloud-resume/tree/main/cookbook'>GitHub</a>,
+                        <div style={ isMobile ? { fontSize:"large" } : {} }
+                             onMouseLeave={ () => { setJokeOption("") } }
+                             onMouseEnter={ () => { setJokeOption("donate") } }>
+                            { jokeOption == "donate" ? <>Don't</> : <>Donate</> }
+                        </div>,
+                        // <div>
+                        //     <pre> Hello: {auth.user?.profile.email} </pre>
+                        //     <pre> ID Token: {auth.user?.id_token} </pre>
+                        //     <pre> Access Token: {auth.user?.access_token} </pre>
+                        //     <pre> Refresh Token: {auth.user?.refresh_token} </pre>
+                        //     <button onClick={() => auth.removeUser()}>Sign out</button>
+                        // </div>,
+                        // }
+                        // return (
+                        //     <>
+                        //         <div>
+                        //             <button onClick={() => auth.signinRedirect()}>Sign in</button>
+                        //             <button onClick={() => signOutRedirect()}>Sign out</button>
+                        //         </div>
+                        //     </>
+                        // );
+                        auth.isAuthenticated
+                            ? <div style={ isMobile ? { fontSize:"large" } : {} }>Hello {auth.user?.profile.email}</div>
+                            : <div style={ isMobile ? { fontSize:"large" } : {} }>You are logged out.</div>,
+                        auth.isAuthenticated
+                            ? <div style={ isMobile ? { fontSize:"large" } : {} } onClick={ () => { auth.removeUser() } }>Sign out</div>
+                            : <div style={ isMobile ? { fontSize:"large" } : {} } onClick={ () => { auth.signinRedirect() } }>Login</div>
+                        ,
+                    ]}
+                >
+                </Sidebar>
+                <div className='flex-column' style={{flexGrow:'1', overflow:'hidden', maxHeight:'100dvh'}}>
+                    {
+                        sidebarOption == "about"
+                            ? <About></About>
+                            : sidebarOption == "new"
+                                ? <NewRecipe></NewRecipe>
+                                : sidebarOption == "browse"
+                                    ? <Browse recipeCallback={fetchRecipe}></Browse>
+                                    : sidebarOption == "recipe" && loading
+                                        ? <Preheating></Preheating>
+                                        : sidebarOption == "recipe"
+                                            ? <>
+                                                <RecipeCard
+                                                    title={data?.title}
+                                                    ingredients={data?.ingredients}
+                                                    items={data?.items}
+                                                    steps={data?.steps}>
+                                                </RecipeCard>
+                                                { error ? <><p style={{color:'red'}}>{error}</p><p style={{color:'darkgoldenrod'}}>Example Recipe</p></> : <></> }
+                                            </>
+                                            : <>Unknown Sidebar Option</>
+                    }
+                    <div style={{width:'100%',textAlign:'center',color:'red',position:'sticky',bottom:'0'}}>
+                        IMPORTANT: Please note that this is a DEV environment - new recipes may be lost any time the DB changes!
+                    </div>
+                </div>
+            </>
+        )
 }
 
