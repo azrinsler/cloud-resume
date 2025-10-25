1a45533dd3e9d81748c680ecf0590624fbc4fd76
Azmo Rinsler <az.rinsler@gmail.com>
2025-10-25 19:26:07 -0400

    making user nullable

--- kotlin-utility/src/main/kotlin/Recipe.kt
+++ kotlin-utility/src/main/kotlin/Recipe.kt
@@ -7,7 +7,7 @@ val logger : Logger = LoggerFactory.getLogger(Recipe::class.java)
 data class Recipe (
     val recipeId: UUID = UUID.randomUUID().also { logger.info("Called to instantiate a Recipe w/ no id. A new id has been randomly generated: $it") },
     val title: String,
-    val user: String,
+    val user: String? = null.also { logger.info("Called to instantiate a Recipe w/ no user. This may cause problems if the user isn't set before attempting to perform an operation on the recipe.") },
     val ingredients: List<Ingredient>,
     val items: List<String>,
     val steps: List<Step>
