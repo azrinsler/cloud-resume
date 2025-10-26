b8914d0f00d8420c1cdb3d335f5692cbdafb0cb2
azrinsler <az.rinsler@gmail.com>
2025-10-26 10:04:39 -0400

    more fiddling with auth

--- cookbook/src/main.tsx
+++ cookbook/src/main.tsx
@@ -3,11 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './css/index.css'
 import { App } from './App.tsx'
 import { AuthProvider } from "react-oidc-context";
-import {UserManager, WebStorageStateStore} from "oidc-client-ts";
+import {SignoutResponse, User, UserManager, WebStorageStateStore} from "oidc-client-ts";
 
 const store = new WebStorageStateStore({ store: window.localStorage })
 // this currently needs to be manually updated to match the user pool any time it gets recreated, which is not ideal
-const cognitoAuthConfig = new UserManager({
+const manager = new UserManager({
     authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_O30KqnNDH",
     client_id: "4beki0cs423atvjm1kr2ojehhu",
     redirect_uri: "https://www.azrinsler.com/",
@@ -18,11 +18,13 @@ const cognitoAuthConfig = new UserManager({
 });
 
 // debug hooks
-const onSignIn = (user: any) => {
-    console.log(`Signed in ${user}`)
-};
-const onSignOut = (user: any) => {
-    console.log(`Sign out ${user}`)
+const onSignIn = (user: User | undefined) => {
+    console.log('onSignIn() User:', user)
+    window.history.replaceState({}, document.title, window.location.pathname)
+}
+
+const onSignOut = (resp: SignoutResponse | undefined) => {
+    console.log(`onSignOut() SignoutResponse:`, resp)
 }
 
 createRoot(document.getElementById('root')!).render(
@@ -30,7 +32,7 @@ createRoot(document.getElementById('root')!).render(
       <AuthProvider
           onSigninCallback = {onSignIn}
           onSignoutCallback={onSignOut}
-          userManager={cognitoAuthConfig}
+          userManager={manager}
       >
           <App />
       </AuthProvider>
