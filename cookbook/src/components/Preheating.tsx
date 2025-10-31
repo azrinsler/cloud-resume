81edf54f4fd1ba8257c27700dfbfe388d4b8cdf7
Azmo Rinsler <az.rinsler@gmail.com>
2025-10-31 17:56:11 -0400

    adding some styling to preheating text

--- cookbook/src/components/Preheating.tsx
+++ cookbook/src/components/Preheating.tsx
@@ -1,9 +1,11 @@
 import * as React from "react";
 
+import '../css/preheating.css'
+
 const Preheating: () => React.JSX.Element = () => {
     return (
-        <div className='flex-column' style={{width:'100%',placeContent:'center',placeItems:'center',flexGrow:'1'}}>
-            <h1>Preheating</h1>
+        <div id="preheating" className='flex-column' style={{width:'100%',placeContent:'center',placeItems:'center',flexGrow:'1'}}>
+            <h1 id="preheating-text">Preheating</h1>
         </div>
     )
 }
